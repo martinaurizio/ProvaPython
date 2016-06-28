f205fc4854135e022b648b9946d834f930c5832e
Martina <martinaurizio@gmail.com>
2016-06-28 14:53:19 +0200

    Aggiunto grafico

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import numpy as np
+import matplotlib.pyplot as plt
 matrice = np.zeros([10,20])
 print(matrice)
-x = np.linspace(0,1,20)
-y = np.linspace(0,1,30)
+x = np.linspace(0,2*np.pi,20)
+y = np.sin(x)
 print(x)
 print(y)
+plt.plot(x,y,'-o')
+plt.show()
