68e3ab20bbdf6eee6d73f1a83a60a7531e93f4ee
LorenzoB <l.baggio@isitgo.it>
2016-06-28 15:54:14 +0200

    es

--- grafico.py
+++ grafico.py
@@ -1,2 +1,9 @@
-def funzione_prova():
-    print 'funzione'
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
+import matplotlib.pyplot as plt
+def superficie(x,y,Z):
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+    [X,Y]=np.meshgrid(x,y)
+    ax.plot_surface(X,Y,Z)
+    plt.show()
